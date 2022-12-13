3cbb0c377c745c722b13516257b9421e89d1f57c
IrinaBazarna <bazarnairen@gmail.com>
2022-12-13 17:44:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
          My react weather app
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
          Bazarna Irina
         </a>
